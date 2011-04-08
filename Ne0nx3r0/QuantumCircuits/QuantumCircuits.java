dc5ab6fc2d984ac04233bb627321983fcf0c721b
Ne0nx3r0 <Ne0nx3r0@gmail.com>
2011-04-07 21:08:48 -0500

    Initial commit

---
+++ Ne0nx3r0/QuantumCircuits/QuantumCircuits.java
@@ -0,0 +1,23 @@
+package Ne0nx3r0.QuantumCircuits;
+
+import org.bukkit.event.Event.Priority;
+import org.bukkit.event.Event;
+import org.bukkit.plugin.PluginDescriptionFile;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.plugin.PluginManager;
+
+public class QuantumCircuits extends JavaPlugin {
+    private final QuantumCircuitsBlockListener blockListener = new QuantumCircuitsBlockListener(this);
+
+    public void onDisable() {
+        System.out.println("Quantum Circuits Disabled");
+    }
+
+    public void onEnable() {
+        PluginManager pm = getServer().getPluginManager();
+        pm.registerEvent(Event.Type.REDSTONE_CHANGE, blockListener, Priority.Normal, this);
+
+        PluginDescriptionFile pdfFile = this.getDescription();
+        System.out.println(pdfFile.getName() + " version " + pdfFile.getVersion() + " ENABLED" );
+    }
+}
---
+++ Ne0nx3r0/QuantumCircuits/QuantumCircuitsBlockListener.java
@@ -0,0 +1,65 @@
+
+package Ne0nx3r0.QuantumCircuits;
+
+import org.bukkit.block.Block;
+import org.bukkit.Material;
+import org.bukkit.event.block.BlockListener;
+import org.bukkit.event.block.BlockRedstoneEvent;
+import org.bukkit.block.BlockFace;
+import org.bukkit.block.Sign;
+
+public class QuantumCircuitsBlockListener extends BlockListener {
+    private final QuantumCircuits plugin;
+
+    public QuantumCircuitsBlockListener(final QuantumCircuits plugin) {
+        this.plugin = plugin;
+    }
+
+    @Override
+    public void onBlockRedstoneChange(BlockRedstoneEvent event) {
+        Block bBlock = event.getBlock();
+
+        BlockFace[] faces = { BlockFace.NORTH, BlockFace.SOUTH, BlockFace.EAST,BlockFace.WEST, BlockFace.UP, BlockFace.DOWN };
+
+        Sign sbBlockSign;
+        
+        for(int i = 0; i < faces.length; i++){
+            if((faces[i] == BlockFace.DOWN && (bBlock.getFace(faces[i],2).getType() == Material.SIGN_POST ||  bBlock.getFace(faces[i],2).getType() == Material.WALL_SIGN)) || bBlock.getFace(faces[i]).getType() == Material.SIGN_POST || bBlock.getFace(faces[i]).getType() == Material.WALL_SIGN){
+                if(faces[i] == BlockFace.DOWN){
+                    sbBlockSign = (Sign) bBlock.getFace(faces[i],2).getState();
+                }else{
+                    sbBlockSign = (Sign) bBlock.getFace(faces[i]).getState();
+                }
+                String[] sBlockLines = sbBlockSign.getLines();
+
+                if(sBlockLines[0].equals("quantum")){
+                    int iActivateX = Integer.parseInt(sBlockLines[1]);
+                    int iActivateY = Integer.parseInt(sBlockLines[2])-1;//To account for the player's +1 shown onscreen
+                    int iActivateZ = Integer.parseInt(sBlockLines[3]);
+
+                    //doesnt account for 0,0,0?
+                    if(iActivateX < 0) iActivateX--;
+                    if(iActivateY < 0) iActivateY--;
+                    if(iActivateZ < 0) iActivateZ--;
+
+                    Block bBlockToActivate = bBlock.getWorld().getBlockAt(iActivateX,iActivateY,iActivateZ);
+
+                    if(bBlockToActivate.getType() == Material.LEVER){
+                        int iData = (int)bBlockToActivate.getData();
+                        if(event.getNewCurrent() > 0){
+                            if((iData&0x08)!=0x08){
+                                    iData|=0x08;
+                            }
+                        }
+                        else{
+                            if((iData&0x08) == 0x08){
+                                iData^=0x08;
+                            }
+                        }
+                        bBlockToActivate.setData((byte) iData);
+                    }
+                }
+            }
+        }
+    }
+}
